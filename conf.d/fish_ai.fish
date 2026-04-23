5d56c6ecaab039f52b05c35b4aab2203e3186329
bndlfm <firefliesandlightningbugs@gmail.com>
2026-04-23 18:22:32 -0500

    Revert "fix: remove remaining -k bind syntax for Fish 4.0+ compatibility"
    This reverts commit 3d321461e921b3622900a92090865ed4ebda086a.

--- conf.d/fish_ai.fish
+++ conf.d/fish_ai.fish
@@ -61,7 +61,11 @@ function _fish_ai_bind --description "Create keybindings for fish-ai."
 
     # Special handling for ctrl-space which might be NULL or nul
     if test "$key2" = "ctrl-space"
-        set key2 nul
+        if string match -r '^[0-3]\.' "$FISH_VERSION" &>/dev/null
+            set key2 -k nul
+        else
+            set key2 nul
+        end
     end
 
     # Remove existing bindings to prevent duplicates or conflicts
